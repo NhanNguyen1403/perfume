01d14a9f802e7f5e2ac873d51bd3e521e2e70c65
Nhan Nguyen <nqnhan1403@gmail.com>
2020-07-06 16:36:23 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,26 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   
   constructor() {
-    let categoryList = []
+    let categoryList = [],
+        brandList = [],
+        currentCategory = '',
+        currentBrand = '',
+        searchContent = ''
   }
   categoryList = ['ALL', 'FAVORITE', 'MEN', 'WOMEN']
+  brandList = ['ALL', 'MONTBLANC', 'DIOR', 'GUCCI', 'CHANNEL', 'TOM FORD', 'LE LABO', 'HUGO']
+  currentCategory = 'ALL'
+  currentBrand = 'ALL'
+  searchContent = ''
 
-
+  changeCategory(newCategory) {
+    this.currentCategory = newCategory
+  }
+  changeBrand(newBrand) {
+    this.currentBrand = newBrand
+  }
+  searchName(event: any) {
+    this.searchContent = event.target.value
+    console.log(this.searchContent)
+  }
 }
