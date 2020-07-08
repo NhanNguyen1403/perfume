c4e6a132b6e243138b9af1c329ed059f75db158c
nnguyen220 <nnguyen220@dxc.com>
2020-07-08 21:06:32 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { ɵangular_packages_platform_browser_dynamic_platform_browser_dynamic_a } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'home',
@@ -13,12 +14,15 @@ export class HomeComponent {
   currentBrand = 'ALL'
   searchContent = ''
   totalPrice = 0
+  filter = ['ALL', 'ALL']
   productList = [
     {
       id: 1,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "MONTB 10 MEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
       gender: 'MEN',
+      views: 10,      
+      brand:'MONTBLANC',
       size: {
         large: {
           quantity: 100,
@@ -56,9 +60,11 @@ export class HomeComponent {
     },
     {
       id: 2,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "DIOR 30 MEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
       gender: 'MEN',
+      views: 30,
+      brand:'DIOR',
       size: {
         large: {
           quantity: 100,
@@ -96,9 +102,11 @@ export class HomeComponent {
     },
     {
       id: 3,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "GUCCI 20 MEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
       gender: 'MEN',
+      views: 20,
+      brand:'GUCCI',
       size: {
         large: {
           quantity: 100,
@@ -136,9 +144,11 @@ export class HomeComponent {
     },
     {
       id: 4,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "CHANNEL 50 WOMEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
-      gender: 'MEN',
+      gender: 'WOMEN',
+      views: 50,
+      brand:'CHANNEL',
       size: {
         large: {
           quantity: 100,
@@ -176,9 +186,11 @@ export class HomeComponent {
     },
     {
       id: 5,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "TF 40 WOMEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
-      gender: 'MEN',
+      gender: 'WOMEN',
+      views: 40,
+      brand:'TOM FORD',
       size: {
         large: {
           quantity: 100,
@@ -216,9 +228,11 @@ export class HomeComponent {
     },
     {
       id: 6,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "LE LA 70 WOMEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
-      gender: 'MEN',
+      gender: 'WOMEN',
+      views: 70,
+      brand:'LE LABO',
       size: {
         large: {
           quantity: 100,
@@ -256,9 +270,11 @@ export class HomeComponent {
     },
     {
       id: 7,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "HUGO 60 WOMEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
-      gender: 'MEN',
+      gender: 'WOMEN',
+      views: 60,
+      brand:'HUGO',
       size: {
         large: {
           quantity: 100,
@@ -296,9 +312,11 @@ export class HomeComponent {
     },
     {
       id: 8,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "MONTB 90 WOMEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
-      gender: 'MEN',
+      gender: 'WOMEN',
+      views: 90,
+      brand:'MONTBLANC',
       size: {
         large: {
           quantity: 100,
@@ -336,9 +354,11 @@ export class HomeComponent {
     },
     {
       id: 9,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "DIOR 80 WOMEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
-      gender: 'MEN',
+      gender: 'WOMEN',
+      views: 80,
+      brand:'DIOR',
       size: {
         large: {
           quantity: 100,
@@ -376,9 +396,11 @@ export class HomeComponent {
     },
     {
       id: 10,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "GUCCI 110 MEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
       gender: 'MEN',
+      views: 110,
+      brand:'GUCCI',
       size: {
         large: {
           quantity: 100,
@@ -416,9 +438,11 @@ export class HomeComponent {
     },
     {
       id: 11,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "CHANNEL 100 MEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
       gender: 'MEN',
+      views: 100,
+      brand:'CHANNEL',
       size: {
         large: {
           quantity: 100,
@@ -456,9 +480,11 @@ export class HomeComponent {
     },
     {
       id: 12,
-      name: "Lorem ipsum, dolor sit amet consectetur adipisicing.",      
+      name: "TF 130 MEN",      
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem officiis incidunt tenetur ipsa velit id voluptates non alias laudantium molestiae rerum, similique enim iusto? Non veniam similique expedita, hic nulla ipsam fugit ab ipsum alias perspiciatis repellat molestiae provident architecto tempore minima odio? Quibusdam maiores eos provident, aperiam dolorum asperiores eius officia! Libero deleniti nam, consequuntur voluptatum dolorum soluta ullam dicta alias eaque assumenda saepe architecto dignissimos totam, repudiandae necessitatibus iusto. Doloribus fugiat dignissimos nulla soluta unde dolor neque sint. ",
       gender: 'MEN',
+      views: 130,
+      brand:'TOM FORD',
       size: {
         large: {
           quantity: 100,
@@ -494,9 +520,24 @@ export class HomeComponent {
         ]
       },
     },
-  ]
+  ]  
+  productListFilter = [...this.productList]
   productChosenList = []
   showProductDetailModal = false
+  productDetail = {
+    photos: {
+      small: [
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+        'https://dummyimage.com/50x50/dddddd/fff.png',
+      ]
+    }
+  }
+  currentproductDetailPhoto = 'https://dummyimage.com/500x500/dddddd/fff.png'
 
 
   toggleModal() {
@@ -504,20 +545,51 @@ export class HomeComponent {
   }
   changeCategory(newCategory) {
     this.currentCategory = newCategory
+    this.currentBrand = 'ALL'
+    this.changeCategoryFilter()
   }
   changeBrand(newBrand) {
     this.currentBrand = newBrand
+    // this.changeBrandFilter()
   }
+  changeCategoryFilter() {
+    if (this.currentCategory == 'ALL') {      
+      this.productListFilter = [...this.productList]
+    } else if (this.currentCategory == 'FAVORITE'){
+      this.productListFilter = [...this.productList]
+      this.productListFilter.sort((a,b) => b.views - a.views)
+    } else if (this.currentCategory == 'MEN') {
+      this.productListFilter = []
+      for (let product of this.productList) {
+        if (product.gender == 'MEN') this.productListFilter.push(product)
+      }
+    } else if (this.currentCategory == 'WOMEN') {
+      this.productListFilter = []
+      for (let product of this.productList) {
+        if (product.gender == 'WOMEN') this.productListFilter.push(product)
+      }
+    }
+    this.brandList =['ALL']
+    for (let product of this.productListFilter) {
+      if (!this.brandList.indexOf(product.brand)) {
+        this.brandList.push(product.brand)
+      }
+    }
+  }
+  change
   searchName(event: any) {
     this.searchContent = event.target.value
     console.log(this.searchContent)
   }
-  horizontalScroll(event: any, isButtonSearch?: boolean) {
-    event.preventDefault()
-    if (isButtonSearch) {
-      event.deltaY > 0 ? event.target.parentElement.scrollLeft += 10 : event.target.parentElement.scrollLeft -= 10
-    } else {
-      event.deltaY > 0 ? event.target.scrollLeft += 10 : event.target.scrollLeft -= 10
+  horizontalScroll(event: any, scrollClass?: any) {
+    event.preventDefault()    
+    let elementList = event.path,
+        scrollUnit = event.deltaY
+
+    for (let element of elementList) {
+      if (element.classList && element.classList[0] == scrollClass) {
+        scrollUnit > 0 ? element.scrollLeft += 20 : element.scrollLeft -= 20
+      }
     }
   }
   addProductToCart(productInfo: any, quantity?: any, size?: any) {    
